df5da6607786832ee1986a83f104dc457b7a5e04
Danya <smetdana12032007@gmail.com>
2024-01-14 02:39:39 +0300

    Skeleton

--- src/components/mainpage/main/headerclothes/card/Card.jsx
+++ src/components/mainpage/main/headerclothes/card/Card.jsx
@@ -1,10 +1,20 @@
-import "./Card.scss";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import SomeComponent from "../../../../../assets/constant";
+import Skeleton from '@mui/material/Skeleton';
 
 function Card({ card, onPlus }) {
     const [isHovered, setIsHovered] = useState(false);
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+    useEffect(() => {
+        const image = new Image();
+        image.src = card.images[0];
+
+        image.onload = () => {
+            setIsImageLoaded(true);
+        };
+    }, [card.images]);
 
     const handleMouseOver = () => {
         setIsHovered(true);
@@ -24,11 +34,10 @@ function Card({ card, onPlus }) {
     const { images } = card;
     const imgSrc = isHovered ? images[1] : images[0];
 
-
     return (
         <div className="card">
             <Link key={card.id} to={`/product-page/${card.id}`}>
-                {images &&  (
+                {isImageLoaded ? (
                     <img
                         className="header__clothes-card-img"
                         src={imgSrc}
@@ -36,6 +45,8 @@ function Card({ card, onPlus }) {
                         onMouseOver={handleMouseOver}
                         onMouseOut={handleMouseOut}
                     />
+                ) : (
+                    <Skeleton animation="wave" width={310} height={310} />
                 )}
                 <p className="price_text"><SomeComponent currentClothing={card}/></p>
             </Link>
--- src/components/mainpage/main/headerclothes/index.jsx
+++ src/components/mainpage/main/headerclothes/index.jsx
@@ -1,36 +1,21 @@
-import { useState, useEffect } from "react";
+
 import "./HeaderClothes.scss";
 import "../../../adaptive/mainpage/MainPageAdaptive.scss"
 import Card from "./card/Card";
-import Skeleton from '@mui/material/Skeleton';
 
 export const HeaderClothes = ({ onPlus, currentItems }) => {
-    const [showImage, setShowImage] = useState(false);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setShowImage(true);
-        }, 5000);
-
-        return () => clearTimeout(timer);
-    }, []);
 
     return (
         <div className="container">
             <section className="header__clothes-card">
-                {currentItems ? (
-                    showImage ? (
-                        currentItems.map((card) => (
-                            <Card
-                                key={card.id}
-                                onPlus={onPlus}
-                                card={card}
-                            />
-                        ))
-                    ) : (
-                        <Skeleton animation="wave" width={310} height={310} />
-                    )
-                ) : null}
+                {currentItems &&
+                    currentItems.map((card) => (
+                        <Card
+                            key={card.id}
+                            onPlus={onPlus}
+                            card={card}
+                        />
+                    ))}
             </section>
         </div>
     );
