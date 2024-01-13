177912613e1b40bad7aea20e1a65fe594f7c9801
Danya <smetdana12032007@gmail.com>
2024-01-14 02:26:46 +0300

    Skeleton

--- src/components/mainpage/main/headerclothes/card/Card.jsx
+++ src/components/mainpage/main/headerclothes/card/Card.jsx
@@ -2,11 +2,9 @@ import "./Card.scss";
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import SomeComponent from "../../../../../assets/constant";
-import { message, Skeleton } from 'antd';
 
 function Card({ card, onPlus }) {
     const [isHovered, setIsHovered] = useState(false);
-    const [isImageLoaded, setIsImageLoaded] = useState(false);
 
     const handleMouseOver = () => {
         setIsHovered(true);
@@ -26,23 +24,17 @@ function Card({ card, onPlus }) {
     const { images } = card;
     const imgSrc = isHovered ? images[1] : images[0];
 
-    const handleImageLoad = () => {
-        setIsImageLoaded(true);
-    };
 
     return (
         <div className="card">
             <Link key={card.id} to={`/product-page/${card.id}`}>
-                {images && !isImageLoaded ? (
-                    <Skeleton variant="rectangular" width={310} height={310} />
-                ) : (
+                {images &&  (
                     <img
                         className="header__clothes-card-img"
                         src={imgSrc}
                         alt=""
                         onMouseOver={handleMouseOver}
                         onMouseOut={handleMouseOut}
-                        onLoad={handleImageLoad}
                     />
                 )}
                 <p className="price_text"><SomeComponent currentClothing={card}/></p>
--- src/components/mainpage/main/headerclothes/index.jsx
+++ src/components/mainpage/main/headerclothes/index.jsx
@@ -1,23 +1,36 @@
-
+import { useState, useEffect } from "react";
 import "./HeaderClothes.scss";
 import "../../../adaptive/mainpage/MainPageAdaptive.scss"
 import Card from "./card/Card";
 import Skeleton from '@mui/material/Skeleton';
+
 export const HeaderClothes = ({ onPlus, currentItems }) => {
+    const [showImage, setShowImage] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowImage(true);
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, []);
 
     return (
         <div className="container">
             <section className="header__clothes-card">
-                {currentItems &&
-                    currentItems.map((card) => (
-                        <Card
-                            key={card.id}
-                            onPlus={onPlus}
-                            card={card}
-                        />
-
-                    ))
-                }
+                {currentItems ? (
+                    showImage ? (
+                        currentItems.map((card) => (
+                            <Card
+                                key={card.id}
+                                onPlus={onPlus}
+                                card={card}
+                            />
+                        ))
+                    ) : (
+                        <Skeleton animation="wave" width={310} height={310} />
+                    )
+                ) : null}
             </section>
         </div>
     );
