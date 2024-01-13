83168863442ed1be58513be32f0485361bb56a6e
Danya <smetdana12032007@gmail.com>
2024-01-14 02:16:32 +0300

    Skeleton

--- src/components/mainpage/main/headerclothes/card/Card.jsx
+++ src/components/mainpage/main/headerclothes/card/Card.jsx
@@ -1,4 +1,4 @@
-import "./Card.scss"
+import "./Card.scss";
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import SomeComponent from "../../../../../assets/constant";
@@ -6,6 +6,7 @@ import { message, Skeleton } from 'antd';
 
 function Card({ card, onPlus }) {
     const [isHovered, setIsHovered] = useState(false);
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
 
     const handleMouseOver = () => {
         setIsHovered(true);
@@ -25,25 +26,26 @@ function Card({ card, onPlus }) {
     const { images } = card;
     const imgSrc = isHovered ? images[1] : images[0];
 
+    const handleImageLoad = () => {
+        setIsImageLoaded(true);
+    };
+
     return (
         <div className="card">
             <Link key={card.id} to={`/product-page/${card.id}`}>
-                {images ? (
+                {images && !isImageLoaded ? (
+                    <Skeleton variant="rectangular" width={310} height={310} />
+                ) : (
                     <img
                         className="header__clothes-card-img"
                         src={imgSrc}
                         alt=""
                         onMouseOver={handleMouseOver}
                         onMouseOut={handleMouseOut}
+                        onLoad={handleImageLoad}
                     />
-                ) : (
-                    <Skeleton variant="rectangular" width={310} height={310} />
                 )}
-                {card ? (
-                    <p className="price_text"><SomeComponent currentClothing={card} /></p>
-                ) : (
-                    <Skeleton variant="text" width={100} />
-                )}
+                <p className="price_text"><SomeComponent currentClothing={card}/></p>
             </Link>
             <div className="card-button-container">
                 <button
