76b09f227ec620cb148c7632f89717a90efae79f
Danya <smetdana12032007@gmail.com>
2024-01-14 04:13:33 +0300

    loading

--- src/components/mainpage/main/headerclothes/card/Card.jsx
+++ src/components/mainpage/main/headerclothes/card/Card.jsx
@@ -1,20 +1,10 @@
 import "./Card.scss";
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import SomeComponent from "../../../../../assets/constant";
-import Skeleton from '@mui/material/Skeleton';
 
 function Card({ card, onPlus }) {
     const [isHovered, setIsHovered] = useState(false);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const img = new Image();
-        img.src = card.images[0];
-        img.onload = () => {
-            setLoading(false);
-        };
-    }, [card.images[0]]);
 
     const handleMouseOver = () => {
         setIsHovered(true);
@@ -26,41 +16,37 @@ function Card({ card, onPlus }) {
 
     const clickHandler = (event) => {
         onPlus(card);
+
         event.preventDefault();
         event.stopPropagation();
     };
 
+    const { images } = card;
+    const imgSrc = isHovered ? images[1] : images[0];
+
+
     return (
-        <div className={`card ${loading ? 'loading' : ''}`}>
-            {loading ? (
-                <div className="loading-skeleton">
-                    <Skeleton variant="rectangular" height={200} width={300} />
-                </div>
-            ) : (
-                <>
-                    <Link key={card.id} to={`/product-page/${card.id}`}>
-                        <div className="image-container">
-                            <img
-                                className="header__clothes-card-img"
-                                src={isHovered ? card.images[1] : card.images[0]}
-                                alt=""
-                                onMouseOver={handleMouseOver}
-                                onMouseOut={handleMouseOut}
-                            />
-
-                        </div>
-                        <p className="price_text">
-                            <SomeComponent currentClothing={card} />
-
-                        </p>
-                    </Link>
-                    <div className="card-button-container">
-                        <button onClick={clickHandler} className="card-button">
-                            Переглянути
-                        </button>
-                    </div>
-                </>
-            )}
+        <div className="card">
+            <Link key={card.id} to={`/product-page/${card.id}`}>
+                {images &&  (
+                    <img
+                        className="header__clothes-card-img"
+                        src={imgSrc}
+                        alt=""
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                    />
+                )}
+                <p className="price_text"><SomeComponent currentClothing={card}/></p>
+            </Link>
+            <div className="card-button-container">
+                <button
+                    onClick={clickHandler}
+                    className="card-button"
+                >
+                    Переглянути
+                </button>
+            </div>
         </div>
     );
 }
